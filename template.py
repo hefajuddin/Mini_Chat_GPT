3acbe4be69ead14239ab2eeca82006034a3af9d0
Hefaj Uddin <hefaj@ecosoftbd.com>
2024-12-05 09:59:45 +0600

    add necessary files

---
+++ template.py
@@ -0,0 +1,27 @@
+import os, sys
+from pathlib import Path
+import logging
+
+listOfFiles=[
+    f"data/chat_data.db",
+    "database.py",
+    "models.py",
+    "requirements.txt"
+    "config.py"
+    "app.py"
+]
+
+for path in listOfFiles:
+    filepath=Path(path)
+    filedir, filename=os.path.split(path)
+
+    if filedir!="":
+        os.makedirs(filedir, exist_ok=True)
+
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, "w") as f:
+            pass
+
+    else:
+        logging.info("file is already present at :{filepath}" )
+
